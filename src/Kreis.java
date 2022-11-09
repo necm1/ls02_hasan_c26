eb332dddb401f8f3677286c239ec70964e0d7a30
Necmi Hasan <hasan@innoscale.de>
2022-11-09 12:31:12 +0100

    UPDATE Kreis.java: finished Task A3.2 #1

--- src/Kreis.java
+++ src/Kreis.java
@@ -1,2 +1,11 @@
+/**
+ * @see https://moodle.oszimt.de/mod/page/view.php?id=451189&forceview=1
+ */
 public class Kreis {
+    public static void main(String[] args) {
+        System.out.printf("%5s\n", "**");
+        System.out.printf("*%6s*\n", "");
+        System.out.printf("*%6s*\n", "");
+        System.out.printf("%5s\n", "**");
+    }
 }
