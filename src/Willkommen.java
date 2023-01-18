818d826a00aabf7a3496d32ff943d39653aa38c5
Necmi Hasan <hasan@innoscale.de>
2023-01-18 12:32:40 +0100

    CREATE Willkommen.java: A6.2.1: Methode: - Willkommen

---
+++ src/Willkommen.java
@@ -0,0 +1,9 @@
+public class Willkommen {
+    public static void main(String[] args) {
+        begreussung();
+    }
+
+    public static void begreussung() {
+        System.out.print("Hallo!");
+    }
+}
