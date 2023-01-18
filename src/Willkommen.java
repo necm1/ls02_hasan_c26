cac22f303cdebf9a44b56345d832b6424b703d74
Necmi Hasan <hasan@innoscale.de>
2023-01-18 12:49:00 +0100

    UPDATE Willkommen.java: A6.2.3: Methode - Geldeinwurf

--- src/Willkommen.java
+++ src/Willkommen.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
 
 public class Willkommen {
+    public static int tickets = 0;
+
+    public static double price =  0.0;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         begreussung();
         double zuZahlenderBetrag = fahrkartenbestellungErfassen(input);
-
-        System.out.printf("Zu zahlender Betrag: %.2f EUR", zuZahlenderBetrag);
+        double eingezahlterGesamtBetrag = fahrkartenBezahlen(input, zuZahlenderBetrag);
     }
 
     public static void begreussung() {
@@ -16,11 +19,28 @@ public class Willkommen {
 
     public static double fahrkartenbestellungErfassen(Scanner input) {
         System.out.print("Ticket Preis: ");
-        double price = input.nextDouble();
+        price = input.nextDouble();
 
         System.out.print("Ticket Anzahl: ");
-        double amount = input.nextDouble();
+        tickets = input.nextInt();
 
-        return price * amount;
+        return price * tickets;
+    }
+
+    public static double fahrkartenBezahlen(Scanner input, double amount) {
+        System.out.printf("Zu zahlender Betrag: %.2f EUR\n", amount);
+
+        double eingezahlterGesamtbetrag = 0.0;
+        double nochZuZahlen = 0.0;
+        while (eingezahlterGesamtbetrag < (price * tickets)) {
+            nochZuZahlen = (price * tickets) - eingezahlterGesamtbetrag;
+
+            System.out.println("Noch zu zahlen: " + String.format("%.2f", nochZuZahlen));
+            System.out.print("Eingabe (mind. 5 Cent, höchstens 2 Euro): ");
+
+            eingezahlterGesamtbetrag = eingezahlterGesamtbetrag + input.nextDouble();
+        }
+
+        return eingezahlterGesamtbetrag;
     }
 }
