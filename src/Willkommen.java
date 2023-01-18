ee153893600ed311c6293d9f121e59cdc0a9a089
Necmi Hasan <hasan@innoscale.de>
2023-01-18 12:38:55 +0100

    UPDATE Willkommen.java: A6.2.2: Methode - Erfassen der Fahrkartenbestellung

--- src/Willkommen.java
+++ src/Willkommen.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 public class Willkommen {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
         begreussung();
+        double zuZahlenderBetrag = fahrkartenbestellungErfassen(input);
+
+        System.out.printf("Zu zahlender Betrag: %.2f EUR", zuZahlenderBetrag);
     }
 
     public static void begreussung() {
-        System.out.print("Hallo!");
+        System.out.println("Hallo!");
+    }
+
+    public static double fahrkartenbestellungErfassen(Scanner input) {
+        System.out.print("Ticket Preis: ");
+        double price = input.nextDouble();
+
+        System.out.print("Ticket Anzahl: ");
+        double amount = input.nextDouble();
+
+        return price * amount;
     }
 }
