897ed0eacb6184cd4e3ccac40321f83d20f5f874
Necmi Hasan <hasan@innoscale.de>
2023-01-18 10:39:13 +0100

    UPDATE Fahrkartenautomat.java: A5.3: Fahrkartenauswahl

--- src/Fahrkartenautomat.java
+++ src/Fahrkartenautomat.java
@@ -12,9 +12,31 @@ class Fahrkartenautomat {
         double nochZuZahlen;
         int tickets;
 
+        Fahrkarte[] fahrkarten = {
+                new Fahrkarte(2.00, "Kurzstrecke AB"),
+                new Fahrkarte(3.00, "Einzelfahrschein AB"),
+                new Fahrkarte(8.80, "Tageskarte AB"),
+                new Fahrkarte(9.40, "4-Fahrten-Karte AB")
+        };
+
+        System.out.println("Fahrkartenbestellvorgang:");
+        System.out.println("=========================");
+        System.out.println("Wählen Sie ihre Wunschfahrkarte für Berlin AB aus:");
+
+        for (int i = 0; i < fahrkarten.length; i++) {
+            Fahrkarte entry = fahrkarten[i];
+
+            // Weil wir nicht von 0 anfangen wollen, addieren wir es mit 1
+            System.out.printf("  %s [%.2f EUR] (%d)\n", entry.getTitle(), entry.getPrice(), i+1);
+        }
+
+        System.out.print("\nIhre Wahl: ");
+        int choice = checkChoice(tastatur.nextInt(), tastatur);
+
         // Geldbetrag eingeben
-        System.out.print("Ticketpreis (Euro): ");
-        zuZahlenderBetrag = tastatur.nextDouble();
+        //System.out.print("Ticketpreis (Euro): ");
+        //zuZahlenderBetrag = tastatur.nextDouble();
+        zuZahlenderBetrag = fahrkarten[choice-1].getPrice();
 
         System.out.print("Anzahl der Tickets: ");
         tickets = checkTicketAmount(tastatur.nextInt(), tastatur);
@@ -97,5 +119,41 @@ class Fahrkartenautomat {
         System.out.println(" >> Wählen Sie bitte eine Anzahl von 10 bis 10 Tickets aus. << ");
         System.out.print("Anzahl der Tickets: ");
         return checkTicketAmount(scanner.nextInt(), scanner);
+    }
+
+    public static int checkChoice(int choice, Scanner scanner) {
+        if (choice <= 0 || choice > 4) {
+            System.out.println(" >>falsche Eingabe<<");
+            System.out.print("Ihre Wahl: ");
+            return checkChoice(scanner.nextInt(), scanner);
+        }
+
+        return choice;
+    }
+}
+
+class Fahrkarte {
+    double price = 0.00;
+    String title = "Fahrkarte";
+
+    public Fahrkarte(double price, String title) {
+        this.price = price;
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 }
